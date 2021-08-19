226f7a7ae6dceb085704e7a279252d5779e26215
Yuri Sanford <75326223+sanfordyuri@users.noreply.github.com>
2021-08-19 13:13:39 -0300

    Exercicio recursividade URI 1028 concluído

--- src/br/com/yuri/Recursividade/URI1028.java
+++ src/br/com/yuri/Recursividade/URI1028.java
@@ -1,9 +1,33 @@
 package br.com.yuri.Recursividade;
 
+import java.util.Scanner;
+
 public class URI1028 {
 
+    public static int mdcRecursivo(int f1, int f2) {
+
+        if(f2 == 0) {
+            return f1;
+        } else {
+            int resto = f1 % f2;
+            f1 = f2;
+            f2 = resto;
+            return mdcRecursivo(f1,f2);
+        }
+
+    }
+
     public static void main(String[] args) {
-        
+
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for (int i=0; i< n; i++) {
+            int f1 = s.nextInt();
+            int f2 = s.nextInt();
+            int mdc = mdcRecursivo(f1, f2);
+            System.out.println(mdc);
+        }
+
     }
 
 }
