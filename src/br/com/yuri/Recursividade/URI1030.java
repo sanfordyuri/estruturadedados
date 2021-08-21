b6fc09f2bc00541d52e00b1770ed466106d1c340
Yuri Sanford <75326223+sanfordyuri@users.noreply.github.com>
2021-08-21 00:32:30 -0300

    Inicializando a classe responsável pelo exercício 1030

---
+++ src/br/com/yuri/Recursividade/URI1030.java
@@ -0,0 +1,17 @@
+package br.com.yuri.Recursividade;
+
+import java.util.Scanner;
+
+public class URI1030 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int nc = s.nextInt();
+
+        for(int i=0; i < nc; i++) {
+            int n = s.nextInt();
+            int k = s.nextInt();
+        }
+    }
+
+}
